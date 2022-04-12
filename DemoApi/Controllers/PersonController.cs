1a03c7bf90a4082b9eec94272ae0593464dd1582
Jeremy Zelaya <jzelaya@krames.com>
2022-04-11 18:21:45 -0600

    Implement MediatR in new web api

---
+++ DemoApi/Controllers/PersonController.cs
@@ -0,0 +1,24 @@
+using DemoLibrary.Models;
+using DemoLibrary.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PersonController : Controller
+{
+    private readonly IMediator _mediator;
+
+    public PersonController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<List<PersonModel>> Get()
+    {
+        return await _mediator.Send(new GetPersonListQuery());
+    }
+}
--- DemoApi/Program.cs
+++ DemoApi/Program.cs
@@ -1,3 +1,7 @@
+using DemoLibrary;
+using DemoLibrary.DataAccess;
+using MediatR;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IDemoDataAccess, DemoDataAccess>();
+builder.Services.AddMediatR(typeof(DemoLibraryMediatREntryPoint).Assembly);
 
 var app = builder.Build();
 
